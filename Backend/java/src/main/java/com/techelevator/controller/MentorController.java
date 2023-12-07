a55b77f5da10c7a37f1330d9484c8982377d8ff7
JeffMuter <muterjeffery@gmail.com>
2023-12-07 07:15:25 -0500

    connected db, added controller, model, jdbcdao for mentor list

--- Backend/java/src/main/java/com/techelevator/controller/MentorController.java
+++ Backend/java/src/main/java/com/techelevator/controller/MentorController.java
@@ -1,13 +1,22 @@
 package com.techelevator.controller;
 
+import com.techelevator.dao.JdbcMentorDao;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 @RestController
 public class MentorController {
 
+    private final JdbcMentorDao jdbcMentorDao;
+
+    public MentorController(JdbcMentorDao jdbcMentorDao) {
+        this.jdbcMentorDao = jdbcMentorDao;
+    }
+
     @GetMapping("/mentors")
-    public String getMentor() {
-        String mentorName = "";
-        return "Welcome to the Mentor page!";
+    public List<String> getMentorNames() {
+        return jdbcMentorDao.getAllMentorNames();
     }
 }
--- Backend/java/src/main/java/com/techelevator/dao/JdbcMentorDao.java
+++ Backend/java/src/main/java/com/techelevator/dao/JdbcMentorDao.java
@@ -1,4 +1,22 @@
 package com.techelevator.dao;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public class JdbcMentorDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcMentorDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<String> getAllMentorNames() {
+        String sql = "SELECT name FROM mentors"; // Adjust this query based on your database schema
+        List<String> mentorNames = jdbcTemplate.query(sql, (rs, rowNum) -> rs.getString("name"));
+        return mentorNames;
+    }
 }
