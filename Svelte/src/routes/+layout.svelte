2c304cdbeec05c78d79ffb7a2c830f3e44433a4e
JeffMuter <muterjeffery@gmail.com>
2023-12-28 21:48:28 -0500

    deleted old code, and added login form and authed protection

--- Svelte/src/routes/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import VoiceRecordButton from './../components/voiceRecordButton.svelte';
-	import Navbar from '../components/navbar.svelte';
-</script>
-
-<Navbar />
-<VoiceRecordButton />
-
-<slot />
---
+++ Svelte/src/routes/login/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+	let isLoginForm = true;
+
+	function toggleForm() {
+		isLoginForm = !isLoginForm;
+	}
+</script>
+
+<main>
+	<div>
+		<h1>{isLoginForm ? 'Login' : 'Register'}</h1>
+		<form>
+			<label for="username">Username:</label><br />
+			<input type="text" id="username" name="username" /><br />
+			<label for="password">Password:</label><br />
+			<input type="password" id="password" name="password" /><br />
+			{#if !isLoginForm}
+				<label for="confirmPassword">Confirm Password:</label><br />
+				<input type="password" id="confirmPassword" name="confirmPassword" /><br />
+			{/if}
+			<input type="submit" value={isLoginForm ? 'Login' : 'Register'} />
+		</form>
+		<button on:click={toggleForm}>{isLoginForm ? 'Switch to Register' : 'Switch to Login'}</button>
+	</div>
+</main>
